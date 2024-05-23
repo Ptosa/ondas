f6200a63c73e73ee2ceb5112ea27e2834c98c4c6
Pedro Lustosa <pedroplustosa@gmail.com>
2024-05-22 23:16:58 -0300

    Mudanças

--- src/pages/Member/Member.jsx
+++ src/pages/Member/Member.jsx
@@ -1,55 +1,64 @@
-import React from 'react'
+import React from 'react';
 import DataSent from '../../components/DataSent/DataSent';
 import Head from '../../hooks/Head/Head';
 import Title from '../../components/Title/Title';
 import Input from '../../components/Input/Input';
 import Button from '../../components/Button/Button';
 import useForm from '../../hooks/useForm/useForm';
-import styles from "./Member.module.css"
+import styles from './Member.module.css';
 import InputRadio from '../../components/InputRadio/InputRadio';
 
 const Member = () => {
+  const name = useForm();
+  const maritalStatus = useForm();
+  const telephone = useForm('telephone');
+  const [form, setForm] = React.useState(null);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
-    const name= useForm();
-    const [form, setForm] = React.useState(null);
-    const handleSubmit = (event)=> {
-        event.preventDefault();
-    }
-
-
-
-  if (form) return <DataSent/>;
+  if (form) return <DataSent />;
 
   return (
     <>
-    <Head
-       title="Ondas - Ilan Church | Membresia"
-       description="Essa é a pagina de Membresia"
-    />
-    <Title>Ficha De Membresia</Title>
+      <Head
+        title="Ondas - Ilan Church | Membresia"
+        description="Essa é a pagina de Membresia"
+      />
+      <Title>Ficha De Membresia</Title>
 
-    <form className={`${styles.form} container`} onSubmit={handleSubmit}><Input
-    label="Nome"
-    id="name"
-    required
-    placeholder="Seu nome."
-    type="text"
-    {...name}/>
-            <p>Estado Civil: </p>
-    <InputRadio
-      options={[
-        { id: 1, name: 'Solteiro' },
-        { id: 2, name: 'Pr Franklin' },
-        { id: 3, name: 'Lídia' },
-        { id: 4, name: 'Silvia' },
-        { id: 5, name: 'Outro' },
-      ]}
-      {...teacher}
+      <form className={`${styles.form} container`} onSubmit={handleSubmit}>
+        <Input
+          label="Nome"
+          id="name"
+          required
+          placeholder="Seu nome."
+          type="text"
+          {...name}
+        />
+        <Input
+          label="Telefone"
+          id="telephone"
+          required
+          placeholder="Seu telefone. Ex: 21980693443"
+          type="tel"
+          {...telephone}
+        />
+        <p>Estado Civil: </p>
+        <InputRadio
+          options={[
+            { id: 1, name: 'Solteiro' },
+            { id: 2, name: 'Casado' },
+            { id: 3, name: 'Lídia' },
+            { id: 4, name: 'Silvia' },
+            { id: 5, name: 'Outro' },
+          ]}
+          {...maritalStatus}
         />
         <Button>Enviar</Button>
-    </form>
+      </form>
     </>
-  )
-  }
+  );
+};
 
-export default Member
+export default Member;
